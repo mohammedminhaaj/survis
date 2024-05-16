3d2ac996b08334e314c365723391a514f1a70ac3
mohammedminhaaj <mohammedminhaaj97@gmail.com>
2024-05-16 22:41:59 +0100

    Making initial changes in a different brnach

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,92 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "cahn2017chatbot": {
+        "author": "Cahn, Jack",
+        "journal": "University of Pennsylvania School of Engineering and Applied Science Department of Computer and Information Science",
+        "title": "CHATBOT: Architecture, design, \\& development",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2017"
+    },
+    "casas2020trends": {
+        "author": "Casas, Jacky and Tricot, Marc-Olivier and Abou Khaled, Omar and Mugellini, Elena and Cudr{\\'e}-Mauroux, Philippe",
+        "booktitle": "Companion Publication of the 2020 International Conference on Multimodal Interaction",
+        "pages": "280--286",
+        "title": "Trends \\& methods in chatbot evaluation",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "chen2020artificial": {
+        "author": "Chen, Lijia and Chen, Pingping and Lin, Zhijian",
+        "journal": "Ieee Access",
+        "pages": "75264--75278",
+        "publisher": "Ieee",
+        "title": "Artificial intelligence in education: A review",
+        "type": "article",
+        "volume": "8",
+        "year": "2020"
+    },
+    "jamalimpact": {
+        "author": "Jamal, Afiya and Pattanaik, Anmol and Gorli, Ravi and Chinmay, Ayes and Jaisai, Tenepalli",
+        "title": "THE IMPACT OF AI CHATBOTS ON TEACHER-STUDENT RELATIONSHIPS IN HIGHER EDUCATION",
+        "type": "article"
+    },
+    "kooli2023chatbots": {
+        "author": "Kooli, Chokri",
+        "journal": "Sustainability",
+        "number": "7",
+        "pages": "5614",
+        "publisher": "MDPI",
+        "title": "Chatbots in education and research: A critical examination of ethical implications and solutions",
+        "type": "article",
+        "volume": "15",
+        "year": "2023"
+    },
+    "labadze2023role": {
+        "author": "Labadze, Lasha and Grigolia, Maya and Machaidze, Lela",
+        "journal": "International Journal of Educational Technology in Higher Education",
+        "number": "1",
+        "pages": "56",
+        "publisher": "Springer",
+        "title": "Role of AI chatbots in education: systematic literature review",
+        "type": "article",
+        "volume": "20",
+        "year": "2023"
+    },
+    "li2023curriculum": {
+        "author": "Li, Yu and Qu, Shang and Shen, Jili and Min, Shangchao and Yu, Zhou",
+        "journal": "arXiv preprint arXiv:2309.16804",
+        "title": "Curriculum-Driven Edubot: A Framework for Developing Language Learning Chatbots Through Synthesizing Conversational Data",
+        "type": "article",
+        "year": "2023"
+    },
+    "mageira2022educational": {
+        "author": "Mageira, Kleopatra and Pittou, Dimitra and Papasalouros, Andreas and Kotis, Konstantinos and Zangogianni, Paraskevi and Daradoumis, Athanasios",
+        "journal": "Applied Sciences",
+        "number": "7",
+        "pages": "3239",
+        "publisher": "MDPI",
+        "title": "Educational AI chatbots for content and language integrated learning",
+        "type": "article",
+        "volume": "12",
+        "year": "2022"
+    },
+    "sharma2021nlp": {
+        "author": "Sharma, Deepak and Paliwal, Mrinal and Rai, Jitender",
+        "journal": "International Journal of Innovative Research in Computer Science \\& Technology",
+        "number": "3",
+        "pages": "179--184",
+        "title": "NLP for Intelligent Conversational Assistance",
+        "type": "article",
+        "volume": "9",
+        "year": "2021"
+    },
+    "studente2020exploring": {
+        "author": "Studente, S and Ellis, S and Garivaldis, SF",
+        "journal": "International Journal of Educational and Pedagogical Sciences",
+        "number": "9",
+        "pages": "768--771",
+        "title": "Exploring the potential of chatbots in higher education: a preliminary study",
+        "type": "article",
+        "volume": "14",
+        "year": "2020"
     }
 };
